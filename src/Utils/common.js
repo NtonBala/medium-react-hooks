9c9383278c49f7a6ecf5c4de87b283845c8158ea
Anton Balashov <balashov.anton.1983@gmail.com>
2023-01-07 13:45:09 +0200

    Add getPaginator utility

--- src/Utils/common.js
+++ src/Utils/common.js
@@ -1,2 +1,12 @@
+import {parse} from 'query-string'
+
 export const range = (start, end) =>
   [...Array(end - start + 1).keys()].map(el => el + start)
+
+export const getPaginator = (search, limit) => {
+  const {page} = parse(search)
+  const currentPage = page ? Number(page) : 1
+  const offset = currentPage * limit - limit
+
+  return {currentPage, offset}
+}
