175e65a88a2744c16700a51d0cfb3c6036bba068
Anton Balashov <balashov.anton.1983@gmail.com>
2023-02-09 13:39:05 +0200

    Fix PopularTags Loading & ErrorMessage CSS

--- src/Components/PopularTags/index.js
+++ src/Components/PopularTags/index.js
@@ -15,23 +15,24 @@ export const PopularTags = () => {
     doFetch()
   }, [doFetch])
 
-  if (isLoading || !response) return <Loading />
-  if (error) return <ErrorMessage />
-
   return (
     <div className="sidebar">
       <p>{t('common.popularTags')}</p>
-      <div className="tag-list">
-        {response.tags.map(tag => (
-          <Link
-            key={tag}
-            to={`${ROUTES.tags}/${tag}`}
-            className="tag-default tag-pill"
-          >
-            {tag}
-          </Link>
-        ))}
-      </div>
+      {isLoading && <Loading />}
+      {error && <ErrorMessage />}
+      {response && (
+        <div className="tag-list">
+          {response.tags.map(tag => (
+            <Link
+              key={tag}
+              to={`${ROUTES.tags}/${tag}`}
+              className="tag-default tag-pill"
+            >
+              {tag}
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
